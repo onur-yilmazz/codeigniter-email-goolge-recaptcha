ae61330b26cca7cbab2ccc6cb1e890b233f40b12
Onur YILMAZ <64357484+onuryilmaz-yzmedia@users.noreply.github.com>
2023-02-01 10:43:55 +0300

    Update mail-form.php

---
+++ mail-form.php
@@ -0,0 +1,46 @@
+                    <form id="form1" name="form1"  method="post" action="<?=base_url(); ?>/send_mail">
+                      <input type="hidden" name="<?php echo $this->security->get_csrf_token_name();?>" value="<?php echo $this->security->get_csrf_hash();?>">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="name"id="name"> 
+                          </div>
+                        </div>
+
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="subject" id="konu">
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="email" class="form-control" name="email" id="email">
+                          </div>
+                        </div>
+
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="phone" id="phone">
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <textarea class="form-control" rows="10" name="message" id="comment"></textarea>
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Google Recaptcha on create 
+                        https://www.google.com/recaptcha/admin/create
+                        reCAPTCHA s2
+                      -->
+                      <div class="g-recaptcha" data-sitekey="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"></div><br>
+
+                      <button type="submit">Send</button>
+                    </form>
